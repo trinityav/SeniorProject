6e4a956a937b00902977259951c2a9a25bd1cd13
SalimAld <salimaldakheel@gmail.com>
2026-02-07 22:29:51 -0600

    Home Screen Update

--- app/src/main/java/com/example/myapplication/HomeScreen.java
+++ app/src/main/java/com/example/myapplication/HomeScreen.java
@@ -1,12 +1,12 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -15,10 +15,23 @@ public class HomeScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+                if (id == R.id.nav_home) {
+                    return true;
+
+                } else if (id == R.id.nav_progress) {
+                    startActivity(new Intent(this, HomeScreen.class));
+                    return true;
+
+                } else if (id == R.id.nav_profile) {
+                    startActivity(new Intent(this, HomeScreen.class));
+                    return true;
+                }
+            return false;
         });
     }
 }
