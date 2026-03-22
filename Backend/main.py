b166e5f8fbe5cbdbf192ec7d6d2b2f3da483d935
edssonzapata90 <edssonzapata99@gmail.com>
2026-03-22 13:27:01 -0500

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,7 @@ from database import SessionLocal, engine
 import models
 import schemas
 from auth.routes import router as auth_router
+from ai_engine import generate_workout_plan
 
 
 #Made changes
@@ -50,3 +51,29 @@ def update_profile(profile: schemas.ProfileUpdate, db: Session = Depends(get_db)
     db.commit()
 
     return {"message": "Profile updated"}
+
+
+# Uses the AI engine to generate a workout plan based on user input
+class AIRequest(BaseModel):
+    age: int
+    days: List[str]
+    duration: int
+    goal: str
+
+
+@app.get("/")
+def home():
+    return {"message": "Backend is running"}
+
+
+@app.post("/ai_plan")
+def ai_plan(request: AIRequest):
+
+    plan = generate_workout_plan(
+        request.age,
+        request.days,
+        request.duration,
+        request.goal
+    )
+
+    return {"plan": plan}
