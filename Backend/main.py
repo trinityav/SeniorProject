2c881c01dff9972cd09d72241e2dad9d95660052
Maisuria-Rohan <MaisuriaR4725@UHCL.edu>
2026-04-09 21:43:57 -0500

    Import Issues

--- Backend/main.py
+++ Backend/main.py
@@ -9,10 +9,10 @@ from sqlalchemy.orm import Session
 from Backend.ai_engine import generate_workout_plan_for_user
 #from Backend.chatbot import answer_fitness_question
 from Backend.database import engine, get_db
-from Backend.dependencies import get_current_user
+from Backend.auth.dependencies import get_current_user
 import Backend.models as models
 import Backend.schemas as schemas
-from Backend.routes import router as auth_router
+from Backend.auth.routes import router as auth_router
 
 models.Base.metadata.create_all(bind=engine)
 
